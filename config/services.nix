5fefdce006893ba0b5527cd4ef0e830f828a0414
rebellehr1010 <riley.lehr@connectsource.com.au>
2025-10-05 19:43:44 +0800

    VPN setup

--- config/services.nix
+++ config/services.nix
@@ -10,6 +10,16 @@
         variant = "";
       };
     };
+    pia = {
+      enable = true;
+      authUserPass = {
+        username = "p8146994";
+        password = "G17DrMiv3&CSe3YgrVV&@i^Rras*I0";
+      };
+      # Alternatively, you can use the `authUserPassFile` attribute if you are using a Nix secrets manager. Here's an example using sops-nix.
+      # The secret you provide to `authUserPassFile` should be a multiline string with a single username on the first line a single password on the second line.
+      # authUserPassFile = config.sops.secrets.pia.path;
+    };
     pipewire = {
       enable = true;
       alsa.enable = true;
--- flake.nix
+++ flake.nix
@@ -10,29 +10,11 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # Zsh plugin sources as flake inputs (non-flake) so we don't need manual sha256 prefetch.
-    zsh-autosuggestions-src.url = "github:zsh-users/zsh-autosuggestions";
-    zsh-autosuggestions-src.flake = false;
-    zsh-completions-src.url = "github:zsh-users/zsh-completions";
-    zsh-completions-src.flake = false;
-    zsh-syntax-highlighting-src.url = "github:zsh-users/zsh-syntax-highlighting";
-    zsh-syntax-highlighting-src.flake = false;
-    zsh-history-substring-search-src.url = "github:zsh-users/zsh-history-substring-search";
-    zsh-history-substring-search-src.flake = false;
-    powerlevel10k-src.url = "github:romkatv/powerlevel10k";
-    powerlevel10k-src.flake = false;
-    nix-zsh-completions-src.url = "github:spwhitt/nix-zsh-completions";
-    nix-zsh-completions-src.flake = false;
-    zsh-command-time-src.url = "github:popstas/zsh-command-time";
-    zsh-command-time-src.flake = false;
-    fzf-tab-src.url = "github:Aloxaf/fzf-tab";
-    fzf-tab-src.flake = false;
-    fzf-zsh-plugin-src.url = "github:unixorn/fzf-zsh-plugin";
-    fzf-zsh-plugin-src.flake = false;
-    you-should-use-src.url = "github:MichaelAquilina/zsh-you-should-use";
-    you-should-use-src.flake = false;
-    zsh-bat-src.url = "github:fdellwing/zsh-bat";
-    zsh-bat-src.flake = false;
+    pia = {
+      url = "github:Fuwn/pia.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
   outputs =
@@ -40,18 +22,16 @@
       self,
       nixpkgs,
       home-manager,
+      pia,
       ...
     }:
     {
-      nixosConfigurations = {
-        nixos-framework = nixpkgs.lib.nixosSystem {
+      nixosConfigurations =
+        let
           system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
+          baseModules = [
             ./config/boot.nix
             ./config/configuration.nix
-            ./config/hosts/nixos-framework/hardware-configuration.nix
-            ./config/hosts/nixos-framework/host.nix
             ./config/hardware.nix
             ./config/internationalisation.nix
             ./config/networking.nix
@@ -62,38 +42,27 @@
             ./config/time.nix
             ./config/users.nix
             home-manager.nixosModules.home-manager
+            pia.nixosModules."x86_64-linux".default
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.riley = import ./home.nix;
             }
           ];
+          mkHost =
+            host: hardwarePath:
+            nixpkgs.lib.nixosSystem {
+              inherit system;
+              specialArgs = { inherit inputs; };
+              modules = baseModules ++ [
+                hardwarePath
+                (./config/hosts + "/" + host + "/host.nix")
+              ];
+            };
+        in
+        {
+          nixos-framework = mkHost "nixos-framework" ./config/hosts/nixos-framework/hardware-configuration.nix;
+          nixos-desktop = mkHost "nixos-desktop" ./config/hosts/nixos-desktop/hardware-configuration.nix;
         };
-        nixos-desktop = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./config/boot.nix
-            ./config/configuration.nix
-            ./config/hosts/nixos-desktop/hardware-configuration.nix
-            ./config/hosts/nixos-desktop/host.nix
-            ./config/hardware.nix
-            ./config/internationalisation.nix
-            ./config/networking.nix
-            ./config/security.nix
-            ./config/services.nix
-            ./config/system-packages.nix
-            ./config/systemd.nix
-            ./config/time.nix
-            ./config/users.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.riley = import ./home.nix;
-            }
-          ];
-        };
-      };
     };
 }

0fef08d92ab900b1a0fc0da8be2895229486545a
rebellehr1010 <riley.lehr@connectsource.com.au>
2025-10-31 16:44:30 +0800

    Revert hardware config for desktop

--- config/hosts/nixos-desktop/hardware-configuration.nix
+++ config/hosts/nixos-desktop/hardware-configuration.nix
@@ -15,23 +15,24 @@
   ];
 
   boot.initrd.availableKernelModules = [
-    "xhci_pci"
-    "thunderbolt"
     "nvme"
+    "xhci_pci"
+    "ahci"
     "usb_storage"
+    "usbhid"
     "sd_mod"
   ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/548f8302-2c54-4a0e-b53a-1eeff7b7ae1d";
+    device = "/dev/disk/by-uuid/581072df-2b06-4268-ac0a-53af304b7904";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/2D53-4640";
+    device = "/dev/disk/by-uuid/D21E-21E2";
     fsType = "vfat";
     options = [
       "fmask=0077"
@@ -39,17 +40,16 @@
     ];
   };
 
-  swapDevices = [
-    { device = "/dev/disk/by-uuid/5e27e7a8-5826-4aa9-87b9-279d4e65a94b"; }
-  ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp170s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp8s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp9s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }

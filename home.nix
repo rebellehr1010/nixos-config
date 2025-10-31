27abdfb88aba4edb8a4b2f25cf6db12e8daaff37
rebellehr1010 <riley.lehr@connectsource.com.au>
2025-10-31 18:20:13 +0800

    WIP gnome-extensions

--- home.nix
+++ home.nix
@@ -5,6 +5,57 @@
   ...
 }:
 {
+
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/desktop/wm/keybindings" = {
+        switch-to-workspace-up = [ ];
+        switch-to-workspace-down = [ ];
+        switch-to-workspace-left = [ ];
+        switch-to-workspace-right = [ ];
+      };
+      "org/gnome/desktop/wm/preferences" = {
+        button-layout = ":minimize,maximize,close";
+      };
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        custom-keybindings = [
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+        ];
+      };
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+        binding = "<Control><Alt>t";
+        command = "kgx";
+        name = "Console";
+      };
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = with pkgs.gnomeExtensions; [
+          "dash-to-dock@micxgx.gmail.com"
+        ];
+      };
+      "org/gnome/shell/extensions/dash-to-dock" = {
+        multi-monitor = true;
+        dock-position = "BOTTOM";
+        dock-fixed = true;
+        extend-height = true;
+        always-center-icons = true;
+        dash-max-icon-size = 40;
+        preview-size-scale = 0.0;
+        show-favorites = true;
+        show-running = true;
+        show-windows-preview = true;
+        workspace-agnostic-urgent-windows = true;
+        scroll-to-focused-applications = true;
+        show-show-apps-button = true;
+        click-action = "minimize-or-preview";
+        custom-theme-shrink = true;
+        disable-overview-on-startup = false;
+        apply-custom-theme = true;
+      };
+    };
+  };
+
   home = {
     username = "riley";
     homeDirectory = "/home/riley";
@@ -94,54 +145,6 @@
     ];
   };
 
-  dconf = {
-    enable = true;
-    settings = {
-      "org/gnome/desktop/wm/keybindings" = {
-        switch-to-workspace-up = [ ];
-        switch-to-workspace-down = [ ];
-        switch-to-workspace-left = [ ];
-        switch-to-workspace-right = [ ];
-      };
-      "org/gnome/desktop/wm/preferences".button-layout = ":minimize,maximize,close";
-      "org/gnome/settings-daemon/plugins/media-keys" = {
-        custom-keybindings = [
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-        ];
-      };
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-        binding = "<Control><Alt>t";
-        command = "kgx";
-        name = "Console";
-      };
-      "org/gnome/shell" = {
-        disable-user-extensions = false;
-        enabled-extensions = with pkgs.gnomeExtensions; [
-          "dash-to-dock@micxgx.gmail.com"
-        ];
-      };
-      "org/gnome/shell/extensions/dash-to-dock" = {
-        multi-monitor = true;
-        dock-position = "BOTTOM";
-        dock-fixed = true;
-        extend-height = true;
-        always-center-icons = true;
-        dash-max-icon-size = 40;
-        preview-size-scale = 0.0;
-        show-favorites = true;
-        show-running = true;
-        show-windows-preview = true;
-        workspace-agnostic-urgent-windows = true;
-        scroll-to-focused-applications = true;
-        show-show-apps-button = true;
-        click-action = "minimize-or-preview";
-        custom-theme-shrink = true;
-        disable-overview-on-startup = false;
-        apply-custom-theme = true;
-      };
-    };
-  };
-
   programs = {
     git = {
       enable = true;
@@ -152,6 +155,12 @@
           name = "rebellehr1010";
         };
       };
+    };
+    gnome-shell = {
+      enable = true;
+      extensions = [
+        { package = pkgs.gnomeExtensions.dash-to-dock; }
+      ];
     };
     home-manager.enable = true;
     zoxide = {

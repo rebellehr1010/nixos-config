bdfe8ba3a7494b71f45b8c1cff689ab5680ee763
rebellehr1010 <riley.lehr@connectsource.com.au>
2025-11-02 12:38:03 +0800

    ethernet driver config

--- config/boot.nix
+++ config/boot.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 {
   boot = {
+    blacklistedKernelModules = [ "r8169" ];
+    extraModulePackages = with pkgs; [ r8125 ];
+    kernelModules = [ "r8125" ];
     kernelPackages = pkgs.linuxPackages_latest;
     loader = {
       systemd-boot.enable = true;

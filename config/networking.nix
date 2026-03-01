69d11490c2480cddbbf21d02b36cfae4dfa12a4a
rebellehr1010 <riley.lehr@connectsource.com.au>
2026-03-01 12:46:04 +0800

    Disable firewall so localsend works

--- config/networking.nix
+++ config/networking.nix
@@ -1,9 +1,13 @@
 { pkgs, ... }:
 {
-  networking.networkmanager = {
-    enable = true;
-    plugins = with pkgs; [
-      networkmanager-openvpn
-    ];
+  networking = {
+    firewall.enable = false;
+    networkmanager = {
+      enable = true;
+      plugins = with pkgs; [
+        networkmanager-openvpn
+      ];
+    };
   };
+
 }

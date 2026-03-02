9f37781de63640d8fb96b9f4e9e55329bfbd6432
rebellehr1010 <riley.lehr@connectsource.com.au>
2026-03-02 09:56:13 +0800

    Add hostname for server

--- config/networking.nix
+++ config/networking.nix
@@ -2,6 +2,9 @@
 {
   networking = {
     firewall.enable = false;
+    hosts = {
+      "192.168.1.71" = [ "riley-server" ];
+    };
     networkmanager = {
       enable = true;
       plugins = with pkgs; [

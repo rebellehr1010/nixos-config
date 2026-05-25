0d64508ae5d562146cc08060b7caac1f6180e4a8
rebellehr1010 <riley.lehr@connectsource.com.au>
2026-05-25 21:56:15 +0800

    Try to fix nautius readis audio/video settings for files

--- config/system-packages.nix
+++ config/system-packages.nix
@@ -1,8 +1,20 @@
-{ pkgs, inputs, ... }:
+{
+  pkgs,
+  inputs,
+  lib,
+  ...
+}:
 {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment = {
+    # in configuration.nix
+    sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
+      pkgs.gst_all_1.gst-plugins-good
+      pkgs.gst_all_1.gst-plugins-bad
+      pkgs.gst_all_1.gst-plugins-ugly
+      pkgs.gst_all_1.gst-libav
+    ];
     systemPackages = with pkgs; [
       inputs.nix-software-center.packages."${pkgs.stdenv.hostPlatform.system}".nix-software-center
       dnsmasq
